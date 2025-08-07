e32ffc10f46f1510dc8cfd336e1b697fef222d33
coderafroj <coderafroj@gmail.com>
2025-08-07 20:07:14 +0530

    add

---
+++ phase1/input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+  printf("Hello Guys\n");
+  int a;
+  float b;
+  char c;
+  // printf("type int\n");
+  // scanf("%d",&a);
+  // printf("type float\n");
+  // scanf("%f",&b);
+  // printf("type char\n");
+  // scanf(" %c",&c);
+  printf("Enter int ,float char\n");
+  scanf("%d %f %c",&a,&b,&c);
+  printf("the value a is %d value b is %f and value c is %c",a,b,c);
+  
+}
---
+++ phase1/variable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+  printf("Variable\n");
+  
+  //Variable declaration
+  int a,b;
+  a=7;  //initiolize
+  float c=8.9;
+  char d='A';
+  
+  printf("number is int %d\n",a);
+  printf("number is float %f\n",c);
+  
+  printf("number %c is chR %c \n",d,d);
+  float sum=a+c;
+  printf("%f",sum);
+  
+  
+    
+}
