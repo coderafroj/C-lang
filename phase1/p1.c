5eda68077d184a2257d7c620d6dbe83b7f7a68ed
coderafroj <coderafroj@gmail.com>
2025-08-11 14:35:48 +0530

    add

---
+++ phase1/p1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  printf("write a program reactangle area\n");
+  int x,y;
+  printf("Enter hight of reatcangle:--\n");
+  scanf("%d",&x);
+  printf("Enter width of reactangle:--\n");
+  scanf("%d",&y);
+  printf("the hight %d and width %d  of reactangle area value %d",x,y,x*y);
+  
+}
